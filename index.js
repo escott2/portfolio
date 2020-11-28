1eacffb730da7499cdc2ac34f590194a55e46792
Emily Scott <62396942+escott2@users.noreply.github.com>
2020-11-28 09:30:31 -0600

    Improve UI

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const flowerSection = document.querySelectorAll(".js-generate-flowers--section")
 const scrollToTopBtn = document.querySelector(".js-scroll-to-top");
 
 
+// let flowerSVG = `
+//     <svg width="40" height="40" viewBox="0 0 261 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <circle cx="131" cy="60" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
+//     <circle cx="201" cy="126" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
+//     <circle cx="60" cy="126" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
+//     <circle cx="131" cy="190" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
+//     <circle cx="131" cy="126" r="46" fill="#5C322D"/>
+//     <circle cx="131" cy="126" r="36" fill="#5C322D"/>
+//     </svg>
+// `
+
 let flowerSVG = `
-    <svg width="40" height="40" viewBox="0 0 261 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="30" height="30" viewBox="0 0 261 250" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="131" cy="60" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
     <circle cx="201" cy="126" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
     <circle cx="60" cy="126" r="53" fill="#5C322D" stroke="black" stroke-width="14"/>
@@ -14,17 +25,22 @@ let flowerSVG = `
     </svg>
 `
 
+// let leafSVG = `
+//     <svg width="20" height="40" viewBox="0 0 126 239" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <path d="M7 103C7 177 64 231 64 231C64 231 119 178 119 103C119 28 64 8 64 8C64 8 7 29 7 103Z" fill="#153337" stroke="black" stroke-width="14"/>
+//     </svg>
+// `
+
 let leafSVG = `
-    <svg width="20" height="40" viewBox="0 0 126 239" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="15" height="35" viewBox="0 0 126 239" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M7 103C7 177 64 231 64 231C64 231 119 178 119 103C119 28 64 8 64 8C64 8 7 29 7 103Z" fill="#153337" stroke="black" stroke-width="14"/>
     </svg>
 `
 
-
 // EVENT LISTENERS ****************************
 
 window.addEventListener('load', () => {
-    generateFlowers(10, flowerHeader);
+    generateFlowers(20, flowerHeader);
 
     for (let i = 0; i < flowerSection.length; i++) {
      generateFlowers(2, flowerSection[i]);
@@ -53,13 +69,14 @@ function generateFlowers(number, location) {
             uniqueLeafSVG = leafSVG.replaceAll("153337", "153337")
             html += uniqueLeafSVG;
             html += flowerSVG;
-
         }
     }
 
-
-
     location.innerHTML = html;
+
+}
+
+function flowerCurve() {
 
 }
 
